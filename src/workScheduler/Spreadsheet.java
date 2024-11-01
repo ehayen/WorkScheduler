88a5c6857403b47db0e23d0ab17f1f297518f4f1
ehayen <ehayen@gmail.com>
2024-11-01 11:40:14 -0700

    Beginning spreadsheet format. Got a single sheet saved with text and cell background changed. Organized all external referenced jars into folder and added to git commit.

--- src/workScheduler/Spreadsheet.java
+++ src/workScheduler/Spreadsheet.java
@@ -1,8 +1,48 @@
 package workScheduler;
+
+/*
+ * Used this site as an example for how to begin using POI
+ * https://www.codejava.net/coding/how-to-write-excel-files-in-java-using-apache-poi
+ */
+
 import org.apache.poi.xssf.usermodel.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 import org.apache.poi.ss.usermodel.*;
 
 public class Spreadsheet
 {
-	Workbook wb = new XSSFWorkbook();
+	public static void main(String[] args)
+	{
+		
+	XSSFWorkbook wb = new XSSFWorkbook();
+	XSSFSheet sheet = wb.createSheet("Sheet 1");
+	Row row = sheet.createRow(0);
+	Cell cell = row.createCell(0);
+	cell.setCellValue("Hello");
+	CellStyle cellStyle = wb.createCellStyle();
+	cellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
+	cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	cell.setCellStyle(cellStyle);
+	
+	try
+	{
+		FileOutputStream outputStream = new FileOutputStream("test.xlsx");
+		wb.write(outputStream);
+		System.out.println("Success");
+	}
+	catch (FileNotFoundException e)
+	{
+		System.out.println("Error");
+		e.printStackTrace();
+	}
+	catch (Exception e)
+	{
+		System.out.println("Exception");
+	}
+	
 }
+}
+
